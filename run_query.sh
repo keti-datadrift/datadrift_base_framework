2362c866019ccdf4fc2d48f371be9dec8e1dc649
jpark <jpark@keti.re.kr>
2024-08-14 11:41:11 +0900

    EVC Framework Snapshot

---
+++ run_query.sh
@@ -0,0 +1,19 @@
+# Find files in source code
+#  by JPark @ 2022, 2023
+
+query=$1
+if [ x$1 == x ]; then
+    query="wandb" 
+fi
+
+echo ----------------------------------------
+echo e.g. sh run_query.sh \"wandb\"
+echo ----------------------------------------
+echo ". Query = " $query
+echo ----------------------------------------
+echo ". Results = " 
+
+grep -R "$query" --include="*.py" ./v2
+
+echo ----------------------------------------
+
