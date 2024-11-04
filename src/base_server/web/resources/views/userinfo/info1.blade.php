5ffc8c5bee073a5847fe943a0d72c52e01393a70
jpark <jpark@keti.re.kr>
2024-11-04 22:36:44 +0900

    Code beauty

--- src/base_server/web/resources/views/userinfo/info1.blade.php
+++ src/base_server/web/resources/views/userinfo/info1.blade.php
@@ -58,13 +58,11 @@
     </style>
 </head>
 <body>
-
     <div class="container">
         <h1><font color="orange">(TODO)</font> 사용자 정보 </h1>
         <!--
         <img src="images/overview/ov1.png" alt="img">
         -->
     </div>
-
 </body>
 </html>
