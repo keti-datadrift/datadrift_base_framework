f8ad18ea10a11f07c4118dc37dc624c51502e000
jpark <jpark@keti.re.kr>
2024-11-04 22:10:51 +0900

    Refactoring

--- run_git_push.sh
+++ run_git_push.sh
@@ -2,7 +2,7 @@ git pull
 git add .
 git status
 #git commit -m "DataDrift Framework Documentation"
-git commit -m "docker refactoring"
+git commit -m "Refactoring"
 #git commit -m "REDIS code update"
 #git commit -m "MariaDB container code update"
 #git commit -m "EVC MariaDB container"
