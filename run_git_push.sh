d2302e74a6d8aed30920f8cae84dfdebbfe923d8
jpark <jpark@keti.re.kr>
2024-11-04 22:34:29 +0900

    Code beauty

--- run_git_push.sh
+++ run_git_push.sh
@@ -2,9 +2,10 @@ git pull
 git add .
 git status
 #git commit -m "DataDrift Framework Documentation"
-git commit -m "Refactoring"
+#git commit -m "Refactoring"
+git commit -m "Code beauty"
 #git commit -m "REDIS code update"
 #git commit -m "MariaDB container code update"
-#git commit -m "EVC MariaDB container"
+#git commit -m "MariaDB container"
 #git commit -m "Documentation update"
 git push
