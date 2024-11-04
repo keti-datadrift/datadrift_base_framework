5390e2136fc90bf220cc0516204ffb1942dec5ae
jpark <jpark@keti.re.kr>
2024-11-04 23:02:49 +0900

    Documentation

--- run_git_push.sh
+++ run_git_push.sh
@@ -1,10 +1,10 @@
 git pull
 git add .
 git status
+git commit -m "Documentation"
 #git commit -m "DataDrift Framework Documentation"
 #git commit -m "Refactoring"
 #git commit -m "App layer snapshot"
-git commit -m "Documentation"
 #git commit -m "REDIS code update"
 #git commit -m "MariaDB container code update"
 #git commit -m "MariaDB container"
