fb8e85134f125147e8f2f9428bd1ebc468df3698
jpark <jpark@keti.re.kr>
2024-11-04 22:45:40 +0900

    App layer snapshot

--- src/base_server/web/routes/web.php
+++ src/base_server/web/routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\IntroController;
 use App\Http\Controllers\TimeSeriesController;
