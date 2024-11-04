82a993f3d605870198c68070dcc19c481aa99643
jpark <jpark@keti.re.kr>
2024-11-04 22:11:41 +0900

    Refactoring

--- src/base_server/web/run_clear_cache.sh
+++ src/base_server/web/run_clear_cache.sh
@@ -1,5 +1,7 @@
+echo "remove cache"
 docker compose run app rm -rf bootstrap/cache/*.php
 docker compose run app php artisan config:clear
 
 echo "recreate cache"
 docker compose run app php artisan config:cache
+
