ffe33b7c5ec7cbb9bd489cdb3a2bc4b46a32b104
jpark <jpark@keti.re.kr>
2024-11-04 22:11:19 +0900

    Refactoring

--- src/base_server/web/run_chmod.sh
+++ src/base_server/web/run_chmod.sh
@@ -2,3 +2,4 @@ docker compose exec app chown -R www-data:www-data /var/www/html/public
 docker compose exec app chmod -R 775 /var/www/html/public 
 docker compose exec app chmod -R 775 /var/www/html/storage
 docker compose exec app chmod -R 775 /var/www/html/bootstrap/cache
+
